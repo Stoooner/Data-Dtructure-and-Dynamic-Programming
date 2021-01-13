dd87d03f56f4ee69bd631c93994f484372bfe5ae
unknown <koushihao@gokemicro.com>
2021-01-13 14:09:54 +0800

    add chapter3 buy_and_sell_stock_1 codes

---
+++ Chapter3/c++ programs/best_time_to_buy_and_sell_stock_1.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    /**
+     * @param prices: Given an integer array
+     * @return: Maximum profit
+     */
+    int maxProfit(vector<int> &prices) {
+        // write your code here
+        // 保底策略：什么都不做，利润0
+        int n = prices.size();
+        if (n == 0){
+            return 0;
+        }
+
+        int result = 0;
+        int min_value = prices[0];// 假定当前最小值是prices[0]
+        for(int i=1;i<n;i++){
+            result = max(result, prices[i]-min_value);
+            min_value = min(min_value, prices[i]); // 时刻维护着min_value是否为最小值
+        }
+
+        return result;
+
+    }
+};
